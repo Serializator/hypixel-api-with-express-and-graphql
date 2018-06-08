4b13a5dfd142ee69a86b640869ab04f70e3822fd
Julian van den Berkmortel <julianvdberkmortel@outlook.com>
2018-06-08 09:43:04 +0200

    Add documentation to the achievement type and make the achievement name non-nullable

--- schema/player/achievement.js
+++ schema/player/achievement.js
@@ -1,11 +1,23 @@
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = require('graphql');
+const { GraphQLObjectType, GraphQLNonNull, GraphQLString, GraphQLInt } = require('graphql');
 
+/**
+ * In the Hypixel API there are two types of achievements.
+ * Achievements that have a score and achievements that you earn once and never again.
+ * In our schema these two types of achievements are represented by the same type.
+ * Achievements that can only be earned once will simply always have a score of 1.
+ * 
+ * GraphQL Type Definition:
+ *     type Achievement {
+ *         name: String!
+*          score: Int
+ *     }
+ */
 module.exports = new GraphQLObjectType({
     name: 'Achievement',
     description: '...',
     fields: () => ({
         name: {
-            type: GraphQLString,
+            type: new GraphQLNonNull(GraphQLString),
             resolve: achievement => achievement.name
         },
         score: {
